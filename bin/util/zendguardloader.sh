46b61d6aabf5b090070993af1a9475c0841e1d0e
mozgbrasil <mozgbrasil@gmail.com>
2017-05-15 17:04:52 -0300

    commit - 15/05/2017 17:04:40

--- bin/util/zendguardloader.sh
+++ bin/util/zendguardloader.sh
@@ -10,7 +10,7 @@ set -eux
 
 DEFAULT_VERSION="LATEST"
 dep_version=${VERSION:-$DEFAULT_VERSION}
-dep_url=
+dep_url=https://github.com/mozgbrasil/storage/blob/master/libs/zend-loader-php5.6-linux-x86_64_update1.tar.gz?raw=true
 dep_dirname=zend-loader-php5.6-linux-x86_64
 
 echo "- Zend Guard Loader installing --> Packaging ext-zend-guard-loader ${DEFAULT_VERSION}..."
